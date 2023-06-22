89378be36b7c288c8b8c4114b6bbac0d4c9fcded
Assane Martial Palm <assane@expat-dakar.com>
2023-06-22 14:50:17 +0000

    update

--- resources/views/admin/countries/index.blade.php
+++ resources/views/admin/countries/index.blade.php
@@ -1,59 +1,111 @@
 <x-master-layout>
-<x-header-immo>
-</x-header-immo>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<div class="">
-<div class="container-xl mt-4">
-	<div class="table-responsive py-5">
-		<div class="table-wrapper">
-			<div class="table-title">
-				<div class="row">
-					<div class="ml-3">
-						<h3 class="">Liste <b>des Indicatifs</b></h3>
-					</div>
-				</div>
-			</div>
-            <table class="table table-striped table-hover"> 
-				<thead>
-					<tr>
-                    <th>{{ __('ID') }}</th>
-                      <th>{{ __('Titre') }}</th>
-                      <th>{{ __('ISO') }}</th>
-                      <th>{{ __('Enabled') }}</th>
-                      <th>{{ __('Indicatif') }}</th>
-                      <th>{{ __('Créé le') }}</th>
-                      <th>{{ __('Updated') }}</th>
-                      <th>{{ __('Actions') }}</th>
-					</tr>
-				</thead>
-				<tbody>
-                         @foreach($countries as $countrie)
-                          <tr>	
-                        <td><a href="{{ route('countrie.show', ['countrie' => $countrie->id]) }}" class="text-decoration-none">
-                            {{$countrie->id}}
-                        </a></td>
-                        <td>{{$countrie->title}}</td>
-                        <td>{{$countrie->iso}}</td>
-                        <td>{{$countrie->enabled ? 'oui' : 'non'}}</td>
-                        <td>{{$countrie->area_code}}</td>
-                        <td>{{ $countrie->created_at->locale('fr_FR')->isoFormat('DD MMM YYYY à HH:mm:ss', 'Do MMM YYYY à HH:mm:ss') }}</td>
-                        <td>{{ $countrie->updated_at->locale('fr_FR')->isoFormat('DD MMM YYYY à HH:mm:ss', 'Do MMM YYYY à HH:mm:ss') }}</td>
-                        <td class="text-nowrap">
-                            <button type="button" class="btn btn-danger" onclick="if(confirm('Êtes-vous sûr de vouloir supprimer ce pays?')) { window.location.href = '{{url('/countrie/delete/'.$countrie->id)}}' }">
-                                <i class="fa fa-trash" aria-hidden="true"></i>
-                            </button>
-                        </td>
-                    </tr>
-                @endforeach
-				</tbody>
-			</table>
-            <div class="pagination">
-            {!! $countries->render() !!}
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header border-0">
+                            <h3 class="card-title">{{ __('Tous les pays') }}</h3>
+                            <div class="card-tools">
+                                <a href="{{ route('countries.create') }}" class="btn btn-tool btn-primary btn-sm">
+                                    <i class="fas fa-plus"></i>
+                                    {{ __('Ajouter') }}
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="card-body">
+                            <div class="table-responsive py-5">
+                                <table class="table table-striped table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>{{ __('#') }}</th>
+                                            <th>{{ __('Nom') }}</th>
+                                            <th>{{ __('Code') }}</th>
+                                            <th>{{ __('Actif') }}</th>
+                                            <th>{{ __('Indicatif') }}</th>
+                                            <th>{{ __('Créé le') }}</th>
+                                            <th>{{ __('Mis à jour le') }}</th>
+                                            <th>{{ __('Actions') }}</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($countries as $country)
+                                            <tr>
+                                                <td>{{ $country->id }}</td>
+                                                <td>{{ $country->title }}</td>
+                                                <td>{{ $country->iso }}</td>
+                                                <td>{{ $country->enabled ? __('Oui') : __('Non')}}</td>
+                                                <td>{{ $country->area_code }}</td>
+                                                <td></td>
+                                                <td></td>
+                                                <td class="text-nowrap">
+                                                    <a
+                                                        href="javascript:;"
+                                                        class="btn btn-info btn-xs"
+                                                        data-toggle="modal" data-target="#country-details-{{ $country->id }}"
+                                                    >
+                                                        <i class="fa fa-eye"></i>
+                                                    </a>
+
+                                                    <!-- Modal -->
+                                                    <div class="modal fade" id="country-details-{{ $country->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                        <div class="modal-dialog" role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title" id="exampleModalLabel">{{ $country->title }}</h5>
+                                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                        <span aria-hidden="true">&times;</span>
+                                                                    </button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    <dl>
+
+                                                                    </dl>
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('Fermer') }}</button>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <a
+                                                        href="{{ route('countries.edit', [
+                                                        'country' => $country->id
+                                                    ]) }}"
+                                                        class="btn btn-primary btn-xs"
+                                                    >
+                                                        <i class="fa fa-pencil"></i>
+                                                    </a>
+
+                                                    <form action="{{ route('countries.delete', [
+                                                        'country' => $country->id
+                                                    ]) }}" method="POST" class="d-inline">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button
+                                                            type="submit"
+                                                            class="btn btn-danger btn-xs"
+                                                            onclick="return confirm(__('Êtes-vous sûr de vouloir supprimer ce pays?'))"
+                                                        >
+                                                            <i class="fa fa-trash" aria-hidden="true"></i>
+                                                        </button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+
+                                <div class="pagination">
+                                    {{ $countries->links() }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-     </div>
-</div>
-</div>
-</div>
-<x-body-immo>
-</x-body-immo>
+        </div>
+    </section>
 </x-master-layout>
