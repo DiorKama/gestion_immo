f09313a51b2a0099bda345aee01d5742ce779a46
Assane Martial Palm <assane@expat-dakar.com>
2023-10-24 16:46:51 +0000

    display session message

---
+++ lang/fr/form.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "added" => "Votre élément a été ajouté avec succès",
+    "deleted" => "L'élément sélectionné a été supprimé avec succès",
+    "enabled" => "L'élément sélectionné a été activé avec succès",
+    "disabled" => "L'élément sélectionné a été désactivé avec succès",
+    "updated" => "Votre élément a été mis à jour avec succès",
+];
--- resources/views/layouts/partials/message.blade.php
+++ resources/views/layouts/partials/message.blade.php
@@ -1,17 +1,26 @@
-@if ($message = Session::get('success'))
-    <div class="container-xl mt-4 py-3">
-        <div class="alert alert-success alert-block">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-                <strong>{{ $message }}</strong>
+@if ($message = Session::get('message'))
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="alert alert-success alert-block">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                    <strong>{{ $message }}</strong>
+                </div>
+            </div>
         </div>
     </div>
 @endif
 
 @if ($message = Session::get('error'))
-    <div class="container-xl mt-4 py-3">
-        <div class="alert alert-danger alert-block">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-                <strong>{{ $message }}</strong>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="alert alert-danger alert-block">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                        <strong>{{ $message }}</strong>
+                </div>
+            </div>
         </div>
     </div>
 @endif
+
